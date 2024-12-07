5efbae255734a97d15d5bc029933759d8744a6bb
Viktorov Ilya <silt777@gmail.com>
2024-12-07 23:26:12 +0300

    edit path 2

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Icon from "@mui/material/Icon";
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
+import MDBox from "./components/MDBox";
 
 // Material Dashboard 2 React example components
 import Sidenav from "examples/Sidenav";
