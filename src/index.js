a60cb7c44c134963a296ee24e09d6141d8b97040
Viktorov Ilya <silt777@gmail.com>
2024-12-07 23:17:33 +0300

    edit path 1

--- src/index.js
+++ src/index.js
@@ -16,10 +16,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "App";
+import App from "@App";
 
 // Material Dashboard 2 React Context Provider
-import { MaterialUIControllerProvider } from "context";
+import { MaterialUIControllerProvider } from "@context";
 import { ApiProvider } from "context";
 
 ReactDOM.render(
