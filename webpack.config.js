7ec1de33762b3e3d0461de273645cba25c8a7935
Viktorov Ilya <silt777@gmail.com>
2024-12-08 11:34:03 +0300

    add relative paths src

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  // Остальные настройки Webpack
-  resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"], // Указываем папку src и node_modules
-    extensions: [".js", ".jsx", ".ts", ".tsx"], // Поддерживаемые расширения
-  },
-};
