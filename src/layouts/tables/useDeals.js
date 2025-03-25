2029f6e6c4928081fcfd7b1c4dc9820f51082f27
Viktorov Ilya <silt777@gmail.com>
2025-03-25 14:52:27 +0300

    log(deal): for prod

--- src/layouts/tables/useDeals.js
+++ src/layouts/tables/useDeals.js
@@ -2,34 +2,35 @@ import { useState, useEffect } from "react";
 import { getDeals } from "./getDeals";
 
 export default function useDeals() {
-    const [deals, setDeals] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [offset, setOffset] = useState(0);
-    const length = 30; // Количество сделок за один запрос
+  const [deals, setDeals] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const length = 30; // Количество сделок за один запрос
 
-    useEffect(() => {
-        loadDeals();
-    }, []);
+  useEffect(() => {
+    loadDeals();
+  }, []);
 
-    async function loadDeals() {
-        if (loading || (total > 0 && deals.length >= total)) return;
+  async function loadDeals() {
+    if (loading || (total > 0 && deals.length >= total)) return;
 
-        setLoading(true);
+    setLoading(true);
 
-        try {
-            const response = await getDeals({}, length, offset);
-            if (response) {
-                setDeals((prevDeals) => [...prevDeals, ...response.deals]);
-                setTotal(response.total);
-                setOffset((prevOffset) => prevOffset + length);
-            }
-        } catch (error) {
-            console.error(`Ошибка загрузки сделок:`, error);
-        } finally {
-            setLoading(false);
-        }
+    try {
+      const response = await getDeals({}, length, offset);
+      console.log(response);
+      if (response) {
+        setDeals((prevDeals) => [...prevDeals, ...response.deals]);
+        setTotal(response.total);
+        setOffset((prevOffset) => prevOffset + length);
+      }
+    } catch (error) {
+      console.error(`Ошибка загрузки сделок:`, error);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return { deals, loadDeals, loading, hasMore: deals.length < total };
+  return { deals, loadDeals, loading, hasMore: deals.length < total };
 }
