2976ff7c4487a52de69296fdbd5c10f55e2183b4
alex karpovich <aakarpovich@mtt.ru>
2025-05-10 14:08:37 +0300

    feat: task_57081 profile add two fetch

--- src/layouts/mainProfile/ui/Profile.jsx
+++ src/layouts/mainProfile/ui/Profile.jsx
@@ -5,22 +5,38 @@ import { Header } from './Header';
 import { Content } from './Content';
 
 import useGetProfile from '../hooks/useGetProfile';
+import Typography from "../../../assets/theme/base/typography";
+import Box from "@mui/material/Box";
+import {CircularProgress} from "@mui/material";
+
 
 export const Profile = () => {
     const data = useGetProfile();
 
-    // console.log('profle data', data);
+    const isLoading = !data || !data.headerData;
 
     return (
         <DashboardLayout>
             <DashboardNavbar />
-            <Header {...data.headerData} />
-            <Content
-                mainInfo={data.mainInfo}
-                schedule={data.schedule}
-                requisites={data.requisites}
-                additionalInfo={data.additionalInfo}
-            />
+
+            {isLoading ? (
+                <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+                    <CircularProgress color="info" />
+                    <Typography variant="body2" color="textSecondary" mt={2}>
+                        Загрузка...
+                    </Typography>
+                </Box>
+            ) : (
+                <>
+                    <Header {...data.headerData} />
+                    <Content
+                        mainInfo={data.mainInfo}
+                        schedule={data.schedule}
+                        requisites={data.requisites}
+                        additionalInfo={data.additionalInfo}
+                    />
+                </>
+            )}
         </DashboardLayout>
     );
 };
